d1c84926030e8d59e8d29330e4592fd1c338133a
Thomas Tuegel <ttuegel@gmail.com>
2014-09-27 21:52:09 -0500

    Remove existing kde-4.14 expressions

--- pkgs/desktops/kde-4.14/kdeedu/artikulate.nix
+++
@@ -1,8 +0,0 @@
-{ kde, kdelibs, qt_gstreamer1 }:
-kde {
-  buildInputs = [ kdelibs qt_gstreamer1 ];
-
-  meta = {
-    description = "Artikulate is a pronunciation learning program for KDE.";
-  };
-}
