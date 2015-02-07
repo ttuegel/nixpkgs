e8888119368b80256376b7c161d7f08e8ae13e33
Thomas Tuegel <ttuegel@gmail.com>
2015-02-07 13:42:51 -0600

    libdbusmenu_qt5: build against split Qt 5

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6020,7 +6020,9 @@ let
   };
 
   libdbusmenu_qt = callPackage ../development/libraries/libdbusmenu-qt { };
-  libdbusmenu_qt5 = callPackage ../development/libraries/libdbusmenu-qt/qt5.nix {};
+  libdbusmenu_qt5 = callPackage ../development/libraries/libdbusmenu-qt/qt5.nix {
+    qt5 = qt5split;
+  };
 
   libdc1394 = callPackage ../development/libraries/libdc1394 { };
 
