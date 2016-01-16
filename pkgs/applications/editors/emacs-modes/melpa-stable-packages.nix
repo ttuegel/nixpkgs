06e031e2800938028a639ee43af1069d417d5495
Thomas Tuegel <ttuegel@gmail.com>
2016-01-16 11:01:06 -0600

    melpaStablePackages: mark broken packages

--- pkgs/applications/editors/emacs-modes/melpa-stable-packages.nix
+++ pkgs/applications/editors/emacs-modes/melpa-stable-packages.nix
@@ -32,6 +32,76 @@ self:
     overrides = {
       # broken upstream
       ack-menu = markBroken super.ack-menu;
+      helm-lobsters = markBroken super.helm-lobsters;
+      powershell = markBroken super.powershell;
+      processing-snippets = markBroken super.processing-snippets;
+
+      # missing: git
+      egg = markBroken super.egg;
+
+      # missing: .NET
+      nemerle = markBroken super.nemerle;
+
+      # missing: ocaml
+      flycheck-ocaml = markBroken super.flycheck-ocaml;
+      merlin = markBroken super.merlin;
+      ocp-indent = markBroken super.ocp-indent;
+      utop = markBroken super.utop;
+
+      # missing: pkg-config
+      crux = markBroken super.crux;
+
+      # part of a larger package
+      notmuch = markBroken super.notmuch;
+
+      # no package header
+      bufshow = markBroken super.bufshow;
+      connection = markBroken super.connection;
+      dictionary = markBroken super.dictionary;
+      elmine = markBroken super.elmine;
+      fold-dwim = markBroken super.fold-dwim;
+      ido-complete-space-or-hyphen = markBroken super.ido-complete-space-or-hyphen;
+      initsplit = markBroken super.initsplit;
+      inflections = markBroken super.inflections;
+      jsfmt = markBroken super.jsfmt;
+      link = markBroken super.link;
+      link-hint = markBroken super.link-hint;
+      maxframe = markBroken super.maxframe;
+      qiita = markBroken super.qiita;
+      speech-tagger = markBroken super.speech-tagger;
+      stgit = markBroken super.stgit;
+      textmate = markBroken super.textmate;
+      voca-builder = markBroken super.voca-builder;
+      whole-line-or-region = markBroken super.whole-line-or-region;
+      window-numbering = markBroken super.window-numbering;
+
+      ac-php = super.ac-php.override {
+        inherit (self.melpaPackages) company popup;
+      };
+
+      airline-themes = super.airline-themes.override {
+        inherit (self.melpaPackages) powerline;
+      };
+
+      easy-kill-extras = super.easy-kill-extras.override {
+        inherit (self.melpaPackages) easy-kill;
+      };
+
+      ess-R-data-view = super.ess-R-data-view.override {
+        inherit (self.melpaPackages) ess ctable popup;
+      };
+
+      ess-R-object-popup = super.ess-R-object-popup.override {
+        inherit (self.melpaPackages) ess popup;
+      };
+
+      mhc = super.mhc.override {
+        inherit (self.melpaPackages) calfw;
+      };
+
+      spaceline = super.spaceline.override {
+        inherit (self.melpaPackages) powerline;
+      };
     };
 
     melpaStablePackages = super // overrides;
