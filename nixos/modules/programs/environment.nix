de3ccd56d17a1d126af55f38304ae58f61eb282e
Thomas Tuegel <ttuegel@gmail.com>
2016-09-11 16:01:30 -0500

    nixos/environment: don't set QT_PLUGIN_PATH

--- nixos/modules/programs/environment.nix
+++ nixos/modules/programs/environment.nix
@@ -38,7 +38,6 @@ in
         PERL5LIB = [ "/lib/perl5/site_perl" ];
         KDEDIRS = [ "" ];
         STRIGI_PLUGIN_PATH = [ "/lib/strigi/" ];
-        QT_PLUGIN_PATH = [ "/lib/qt4/plugins" "/lib/kde4/plugins" ];
         QTWEBKIT_PLUGIN_PATH = [ "/lib/mozilla/plugins/" ];
         GTK_PATH = [ "/lib/gtk-2.0" "/lib/gtk-3.0" ];
         XDG_CONFIG_DIRS = [ "/etc/xdg" ];
