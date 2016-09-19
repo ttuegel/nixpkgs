0b9cf870f6bd8335cfcea6c2eb1b3f33d1540f30
Thomas Tuegel <ttuegel@gmail.com>
2016-09-19 18:07:37 -0500

    kdeWrapper: use QT5_PLUGIN_PATH

--- pkgs/development/libraries/kde-frameworks/kde-wrapper.nix
+++ pkgs/development/libraries/kde-frameworks/kde-wrapper.nix
@@ -28,9 +28,9 @@ stdenv.mkDerivation {
                 --suffix PATH : "$out/bin:$env/bin" \
                 --prefix XDG_CONFIG_DIRS : "$env/etc/xdg" \
                 --prefix XDG_DATA_DIRS : "$env/share" \
-                --set QML_IMPORT_PATH "$env/lib/qt5/imports" \
-                --set QML2_IMPORT_PATH "$env/lib/qt5/qml" \
-                --set QT_PLUGIN_PATH "$env/lib/qt5/plugins"
+                --prefix QML_IMPORT_PATH "$env/lib/qt5/imports" \
+                --prefix QML2_IMPORT_PATH "$env/lib/qt5/qml" \
+                --prefix QT5_PLUGIN_PATH "$env/lib/qt5/plugins"
         else
             echo "no such file or directory: $drv/$t"
             exit 1
