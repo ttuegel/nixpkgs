f64ef66511061894fb4fd707b81cf9a1fb6ee0c2
Thomas Tuegel <ttuegel@gmail.com>
2017-02-04 09:49:36 -0600

    kde5.systemsettings: remove unused dependency on makeQtWrapper

--- pkgs/desktops/kde-5/plasma/systemsettings.nix
+++ pkgs/desktops/kde-5/plasma/systemsettings.nix
@@ -1,12 +1,12 @@
 {
-  plasmaPackage, ecm, kdoctools, makeQtWrapper,
+  plasmaPackage, ecm, kdoctools,
   kcmutils, kconfig, kdbusaddons, khtml, ki18n, kiconthemes, kio, kitemviews,
   kservice, kwindowsystem, kxmlgui, qtquickcontrols
 }:
 
 plasmaPackage {
   name = "systemsettings";
-  nativeBuildInputs = [ ecm kdoctools makeQtWrapper ];
+  nativeBuildInputs = [ ecm kdoctools ];
   propagatedBuildInputs = [
     kcmutils kconfig kdbusaddons khtml ki18n kiconthemes kio kitemviews kservice
     kwindowsystem kxmlgui qtquickcontrols
