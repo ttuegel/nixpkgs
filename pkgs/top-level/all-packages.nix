d489582f4a25f26ac73d06fadd5bcf350d411c03
Thomas Tuegel <ttuegel@mailbox.org>
2017-02-17 10:02:25 -0600

    kde4: remove rekonq
    This KDE 4-based browser is unmaintained upstream and has not even received
    security updates in several years.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16666,9 +16666,6 @@ with pkgs;
 
           psi = callPackage ../applications/networking/instant-messengers/psi { };
 
-          rekonq-unwrapped = callPackage ../applications/networking/browsers/rekonq { };
-          rekonq = wrapFirefox rekonq-unwrapped { };
-
           kwebkitpart = callPackage ../applications/networking/browsers/kwebkitpart { };
 
           semnotes = callPackage ../applications/misc/semnotes { };
@@ -17793,8 +17790,6 @@ with pkgs;
     webkit = webkitgtk2;
   };
   vimprobable2 = wrapFirefox vimprobable2-unwrapped { };
-
-  inherit (kde4) rekonq;
 
   vimb-unwrapped = callPackage ../applications/networking/browsers/vimb {
     webkit = webkitgtk2;
