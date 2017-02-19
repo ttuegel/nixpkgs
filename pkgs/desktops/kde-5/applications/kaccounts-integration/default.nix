0a66f7b84136ef61fac201b16b1627abee4fb745
Thomas Tuegel <ttuegel@mailbox.org>
2017-02-19 09:42:55 -0600

    fixup! kaccounts-integration: init at 16.12.1

--- pkgs/desktops/kde-5/applications/kaccounts-integration/default.nix
+++ pkgs/desktops/kde-5/applications/kaccounts-integration/default.nix
@@ -9,11 +9,10 @@ kdeApp {
   name = "kaccounts-integration";
   meta = { license = with lib.licenses; [ gpl2 ]; };
   nativeBuildInputs = [ ecm intltool ];
-  buildInputs = [
-    kcmutils ki18n kcoreaddons kdbusaddons kdeclarative kpackage
-    libaccounts-glib qtbase
+  propagatedBuildInputs = [
+    accounts-qt5 kcmutils ki18n kcoreaddons kdbusaddons kdeclarative kpackage
+    libaccounts-glib libsignon-qt5 qtbase
   ];
-  propagatedBuildInputs = [ accounts-qt5 libsignon-qt5 ];
 
   postFixup = ''
     rm "''${!outputDev}/lib/cmake/KAccounts/FindAccountsFileDir.cmake"
