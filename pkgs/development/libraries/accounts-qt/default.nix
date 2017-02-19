554afe9b8b51a754298ed9602172de48ec08c893
Thomas Tuegel <ttuegel@mailbox.org>
2017-02-18 21:00:20 -0600

    accounts-qt: 1.13 -> 1.15

--- pkgs/development/libraries/accounts-qt/default.nix
+++ pkgs/development/libraries/accounts-qt/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "accounts-qt-${version}";
-  version = "1.13";
+  version = "1.15";
 
   src = fetchFromGitLab {
-    sha256 = "1gpkgw05dwsf2wk5cy3skgss3kw6mqh7iv3fadrxqxfc1za1xmyl";
-    rev = version;
+    sha256 = "0cnra7g2mcgzh8ykrj1dpb4khkx676pzdr4ia1bvsp0cli48691w";
+    rev = "VERSION_" + version;
     repo = "libaccounts-qt";
     owner = "accounts-sso";
   };
@@ -20,7 +20,7 @@ stdenv.mkDerivation rec {
 
   meta = with stdenv.lib; {
     description = "Qt library for accessing the online accounts database";
-    homepage = "http://code.google.com/p/accounts-sso/";
+    homepage = "https://gitlab.com/accounts-sso/libaccounts-qt";
     license = licenses.lgpl21;
     maintainers = with maintainers; [ nckx ];
     platforms = with platforms; linux;
