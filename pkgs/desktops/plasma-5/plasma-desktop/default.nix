043ebbe7a9375e112713ba63a62d676daf7e834d
Thomas Tuegel <ttuegel@mailbox.org>
2017-03-17 17:48:45 -0500

    plasma-desktop: fix kimpanel
    kimpanel needs to be wrapped because it will be called outside a Qt
    application.

--- pkgs/desktops/plasma-5/plasma-desktop/default.nix
+++ pkgs/desktops/plasma-5/plasma-desktop/default.nix
@@ -1,6 +1,6 @@
 {
   plasmaPackage, lib, copyPathsToStore,
-  extra-cmake-modules, kdoctools,
+  extra-cmake-modules, kdoctools, makeQtWrapper,
   attica, baloo, boost, fontconfig, ibus, kactivities, kactivities-stats, kauth,
   kcmutils, kdbusaddons, kdeclarative, kded, kdelibs4support, kemoticons,
   kglobalaccel, ki18n, kitemmodels, knewstuff, knotifications, knotifyconfig,
@@ -13,7 +13,7 @@
 
 plasmaPackage rec {
   name = "plasma-desktop";
-  nativeBuildInputs = [ extra-cmake-modules kdoctools ];
+  nativeBuildInputs = [ extra-cmake-modules kdoctools makeQtWrapper ];
   buildInputs = [
     attica boost fontconfig ibus kcmutils kdbusaddons kded kitemmodels knewstuff
     knotifications knotifyconfig kwallet libcanberra_kde libXcursor
@@ -31,14 +31,21 @@ plasmaPackage rec {
 
     sed '1i#include <cmath>' -i kcms/touchpad/src/backends/x11/synapticstouchpad.cpp
   '';
+
   NIX_CFLAGS_COMPILE = [ "-I${xorgserver.dev}/include/xorg" ];
   cmakeFlags = [
     "-DEvdev_INCLUDE_DIRS=${xf86inputevdev.dev}/include/xorg"
     "-DSynaptics_INCLUDE_DIRS=${xf86inputsynaptics.dev}/include/xorg"
   ];
+
   postInstall = ''
     # Display ~/Desktop contents on the desktop by default.
+    # This will be the default in Plasma 5.10.
     sed -i "$out/share/plasma/shells/org.kde.plasma.desktop/contents/defaults" \
         -e 's/Containment=org.kde.desktopcontainment/Containment=org.kde.plasma.folder/'
+
+    wrapQtProgram $out/lib/libexec/kimpanel \
+        --prefix XDG_DATA_DIRS : "$GSETTINGS_SCHEMAS_PATH" \
+        --prefix GI_TYPELIB_PATH : "$GI_TYPELIB_PATH"
   '';
 }
