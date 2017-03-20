6377114ce25ca536a707804c28dbfa4c08f12921
Thomas Tuegel <ttuegel@mailbox.org>
2017-03-20 08:28:37 -0500

    extra-cmake-modules: disable automatic XDG_*_DIRS propagation

--- pkgs/development/libraries/kde-frameworks/extra-cmake-modules/setup-hook.sh
+++ pkgs/development/libraries/kde-frameworks/extra-cmake-modules/setup-hook.sh
@@ -5,47 +5,6 @@ ecmFindXdgBuildPaths() {
 
 envHooks+=(ecmFindXdgBuildPaths)
 
-ecmSharedDataDirs=( \
-    doc/HTML \
-    config.kcfg \
-    kconf_update \
-    kservices5 \
-    kservicetypes5 \
-    kxmlgui5 \
-    knotifications5 \
-    icons \
-    locale \
-    sounds \
-    templates \
-    wallpapers \
-    applications \
-    desktop-directories \
-    mime \
-    appdata \
-    man \
-    info \
-    dbus-1 \
-)
-
-ecmFindXdgRuntimePaths() {
-    for d in ${ecmSharedDataDirs[*]}; do
-        if [ -d "$1/share/$d" ]; then
-            makeWrapperArgs+=(--prefix XDG_DATA_DIRS : "$1/share")
-            break
-        fi
-    done
-
-    if [ -d "$1/etc/xdg" ]; then
-        makeWrapperArgs+=(--prefix XDG_CONFIG_DIRS : "$1/etc/xdg")
-    fi
-}
-
-if [ -z "$crossConfig" ]; then
-    envHooks+=(ecmFindXdgRuntimePaths)
-else
-    crossEnvHooks+=(ecmFindXdgRuntimePaths)
-fi
-
 ecmSetCMakeFlags() {
     # Because we need to use absolute paths here, we must set *all* the paths.
     cmakeFlags+=" -DKDE_INSTALL_EXECROOTDIR=${!outputBin}"
