c2d968121fe8baa6466254e9e633e7e45665fc9b
Thomas Tuegel <ttuegel@mailbox.org>
2017-03-24 13:30:16 -0500

    smtube: pin to Qt 5.6

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14728,7 +14728,7 @@ with pkgs;
 
   smplayer = libsForQt5.callPackage ../applications/video/smplayer { };
 
-  smtube = libsForQt55.callPackage ../applications/video/smtube {};
+  smtube = libsForQt56.callPackage ../applications/video/smtube {};
 
   sudolikeaboss = callPackage ../tools/security/sudolikeaboss { };
 
