42b959356ba23257e62d80d441c138df962f41ad
Thomas Tuegel <ttuegel@mailbox.org>
2017-05-09 09:20:59 -0500

    breeze-gtk: Patch builder to find GTK2 pimap engine
    This does not seem to be strictly necessary, but it also cannot hurt.

--- pkgs/desktops/plasma-5/breeze-gtk.nix
+++ pkgs/desktops/plasma-5/breeze-gtk.nix
@@ -1,11 +1,17 @@
-{ plasmaPackage
+{ plasmaPackage, lib
 , extra-cmake-modules
-, qtbase
+, gtk2, qtbase
 }:
+
+let inherit (lib) getLib; in
 
 plasmaPackage {
   name = "breeze-gtk";
   nativeBuildInputs = [ extra-cmake-modules ];
   cmakeFlags = [ "-DWITH_GTK3_VERSION=3.22" ];
   buildInputs = [ qtbase ];
+  postPatch = ''
+    sed -i cmake/FindGTKEngine.cmake \
+      -e "s|\''${KDE_INSTALL_FULL_LIBDIR}|${getLib gtk2}/lib|"
+  '';
 }
