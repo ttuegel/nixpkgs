a84e2ac2959ed4e8332696d6a3ed3de80af1649c
Thomas Tuegel <ttuegel@mailbox.org>
2017-05-16 10:56:41 -0500

    Remove kdeWrapper

--- pkgs/applications/misc/krename/default.nix
+++ pkgs/applications/misc/krename/default.nix
@@ -1,35 +1,30 @@
 {
-  mkDerivation, kdeWrapper, fetchFromGitHub, lib,
-  extra-cmake-modules, kdoctools, kconfig, kinit, kjsembed,
-  taglib, exiv2, podofo
+  mkDerivation, fetchFromGitHub, lib,
+  extra-cmake-modules, kdoctools, wrapGAppsHook,
+  kconfig, kinit, kjsembed, taglib, exiv2, podofo
 }:
 
 let
   pname = "krename";
   version = "20161228";
-  unwrapped = mkDerivation rec {
-    name = "${pname}-${version}";
+in mkDerivation rec {
+  name = "${pname}-${version}";
 
-    src = fetchFromGitHub {
-      owner  = "KDE";
-      repo   = "krename";
-      rev    = "4e55c2bef50898eb4a6485ce068379b166121895";
-      sha256 = "09yz3sxy2l6radfybkj2f7224ggf315vnvyksk0aq8f03gan6cbp";
-    };
-
-    meta = with lib; {
-      homepage = http://www.krename.net;
-      description = "A powerful batch renamer for KDE";
-      inherit (kconfig.meta) platforms;
-      maintainers = with maintainers; [ peterhoeg ];
-    };
-
-    buildInputs = [ taglib exiv2 podofo ];
-    nativeBuildInputs = [ extra-cmake-modules kdoctools ];
-    propagatedBuildInputs = [ kconfig kinit kjsembed ];
+  src = fetchFromGitHub {
+    owner  = "KDE";
+    repo   = "krename";
+    rev    = "4e55c2bef50898eb4a6485ce068379b166121895";
+    sha256 = "09yz3sxy2l6radfybkj2f7224ggf315vnvyksk0aq8f03gan6cbp";
   };
 
-in kdeWrapper {
-  inherit unwrapped;
-  targets = [ "bin/krename" ];
+  meta = with lib; {
+    homepage = http://www.krename.net;
+    description = "A powerful batch renamer for KDE";
+    inherit (kconfig.meta) platforms;
+    maintainers = with maintainers; [ peterhoeg ];
+  };
+
+  buildInputs = [ taglib exiv2 podofo ];
+  nativeBuildInputs = [ extra-cmake-modules kdoctools wrapGAppsHook ];
+  propagatedBuildInputs = [ kconfig kinit kjsembed ];
 }
--- pkgs/applications/misc/yakuake/default.nix
+++ pkgs/applications/misc/yakuake/default.nix
@@ -2,7 +2,7 @@
 , lib
 , fetchurl
 , kdoctools
-, kdeWrapper
+, wrapGAppsHook
 , extra-cmake-modules
 , karchive
 , kcrash
@@ -18,46 +18,38 @@
 }:
 
 let
-  unwrapped = let
-    pname = "yakuake";
-    version = "3.0.2";
-  in mkDerivation rec {
-    name = "${pname}-${version}";
+  pname = "yakuake";
+  version = "3.0.2";
+in mkDerivation rec {
+  name = "${pname}-${version}";
 
-    src = fetchurl {
-      url = "http://download.kde.org/stable/${pname}/${version}/src/${name}.tar.xz";
-      sha256 = "0vcdji1k8d3pz7k6lkw8ighkj94zff2l2cf9v1avf83f4hjyfhg5";
-    };
-
-    buildInputs = [
-      karchive
-      kcrash
-      kdbusaddons
-      ki18n
-      kiconthemes
-      knewstuff
-      knotifications
-      knotifyconfig
-      kparts
-      kwindowsystem
-    ];
-
-    nativeBuildInputs = [
-      extra-cmake-modules kdoctools
-    ];
-
-    meta = {
-      homepage = https://yakuake.kde.org;
-      description = "Quad-style terminal emulator for KDE";
-      maintainers = with lib.maintainers; [ fridh ];
-    };
+  src = fetchurl {
+    url = "http://download.kde.org/stable/${pname}/${version}/src/${name}.tar.xz";
+    sha256 = "0vcdji1k8d3pz7k6lkw8ighkj94zff2l2cf9v1avf83f4hjyfhg5";
   };
 
+  propagatedBuildInputs = [
+    karchive
+    kcrash
+    kdbusaddons
+    ki18n
+    kiconthemes
+    knewstuff
+    knotifications
+    knotifyconfig
+    kparts
+    kwindowsystem
+  ];
 
-in
-kdeWrapper
-{
-  inherit unwrapped;
-  targets = [ "bin/yakuake" ];
-  paths = [ konsole.unwrapped ];
+  propagatedUserEnvPkgs = [ konsole ];
+
+  nativeBuildInputs = [
+    extra-cmake-modules kdoctools wrapGAppsHook
+  ];
+
+  meta = {
+    homepage = https://yakuake.kde.org;
+    description = "Quad-style terminal emulator for KDE";
+    maintainers = with lib.maintainers; [ fridh ];
+  };
 }
