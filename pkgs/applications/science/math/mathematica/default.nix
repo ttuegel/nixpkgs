f3857a8bb6362e03a8c863840cda2191dc07cc82
Thomas Tuegel <ttuegel@mailbox.org>
2017-07-12 12:44:43 -0500

    mathematica: reduce system dependencies
    It is important to reduce system dependencies for proprietary software by
    removing libraries provided by the vendor whenever possible. While the vendor
    may ship libraries with the same soname as system libraries, we cannot determine
    if or how the vendor has patched those libraries.
    
    In our Nix expression for Mathematica, the system dependencies are substituted
    by several patchelf incantations. The script responsible for this process was
    cleaned up during this change, although its function was left substatially
    unaltered.

--- pkgs/applications/science/math/mathematica/default.nix
+++ pkgs/applications/science/math/mathematica/default.nix
@@ -1,30 +1,21 @@
-{ stdenv
-, coreutils
-, patchelf
-, requireFile
-, alsaLib
-, fontconfig
-, freetype
-, gcc
-, glib
-, libpng
-, ncurses
-, opencv
-, openssl
-, unixODBC
-, xorg
-, zlib
-, libxml2
-, libuuid
+{
+  stdenv, patchelf, requireFile,
+  alsaLib, atk, clucene_core_2, ffmpeg_0_10, ffmpeg_1, fluidsynth, fontconfig,
+  freetype, gcc, giflib, gtk2, icu_54, lame, libidn, libpng12, libsndfile,
+  libuuid, libxml2, libxslt, mesa_glu, ncurses, R, xorg,
 }:
 
+let fontconfig_ = fontconfig; freetype_ = freetype; in
+
 let
-  platform =
-    if stdenv.system == "i686-linux" || stdenv.system == "x86_64-linux" then
-      "Linux"
-    else
-      throw "Mathematica requires i686-linux or x86_64 linux";
+  fontconfig = fontconfig_.override { inherit freetype; };
+  freetype = freetype_.override { inherit libpng; };
+  libpng = libpng12;
+  libXft = xorg.libXft.override {
+    inherit fontconfig freetype libpng;
+  };
 in
+
 stdenv.mkDerivation rec {
   version = "11.0.1";
 
@@ -32,7 +23,7 @@ stdenv.mkDerivation rec {
 
   src = requireFile rec {
     name = "Mathematica_${version}_LINUX.sh";
-    message = '' 
+    message = ''
       This nix expression requires that ${name} is
       already part of the store. Find the file on your Mathematica CD
       and add it to the nix store with nix-store --add-fixed sha256 <FILE>.
@@ -40,100 +31,156 @@ stdenv.mkDerivation rec {
     sha256 = "1qqwz8gbw74rnnyirpbdanwx3d25s4x0i4zc7bs6kp959x66cdkw";
   };
 
-  buildInputs = [
-    coreutils
-    patchelf
-    alsaLib
-    coreutils
-    fontconfig
-    freetype
-    gcc.cc
-    gcc.libc
-    glib
-    ncurses
-    opencv
-    openssl
-    unixODBC
-    libxml2
-    libuuid
-  ] ++ (with xorg; [
-    libX11
-    libXext
-    libXtst
-    libXi
-    libXmu
-    libXrender
-    libxcb
-    libXcursor
-    libXfixes
-    libXrandr
-    libICE
-    libSM
-  ]);
+  nativeBuildInputs = [ patchelf ];
 
-  ldpath = stdenv.lib.makeLibraryPath buildInputs
+  buildInputs = with xorg; [
+    alsaLib atk clucene_core_2 ffmpeg_0_10 ffmpeg_1 fluidsynth gcc.cc gcc.libc
+    giflib gtk2 fontconfig icu_54 lame libICE libSM libX11 libXcursor libXext
+    libXfixes libXft libXi libXmu libXrandr libXrender libXtst libXxf86vm libidn
+    libsndfile libuuid libxcb libxml2 libxslt mesa_glu R ncurses
+  ];
+
+  ldpath =
+    stdenv.lib.makeLibraryPath buildInputs
     + stdenv.lib.optionalString (stdenv.system == "x86_64-linux")
       (":" + stdenv.lib.makeSearchPathOutput "lib" "lib64" buildInputs);
 
-  phases = "unpackPhase installPhase fixupPhase";
+  builder = builtins.toFile "builder.sh" ''
+    . $stdenv/setup
 
-  unpackPhase = ''
-    echo "=== Extracting makeself archive ==="
-    # find offset from file
-    offset=$(${stdenv.shell} -c "$(grep -axm1 -e 'offset=.*' $src); echo \$offset" $src)
-    dd if="$src" ibs=$offset skip=1 | tar -xf -
-    cd Unix
-  '';
+    # Exit immediately upon error
+    set -e
 
-  installPhase = ''
-    cd Installer
-    # don't restrict PATH, that has already been done
-    sed -i -e 's/^PATH=/# PATH=/' MathInstaller
-    sed -i -e 's/\/bin\/bash/\/bin\/sh/' MathInstaller
+    echo -n "Extracting \`${src.name}'..."
 
-    echo "=== Running MathInstaller ==="
-    ./MathInstaller -auto -createdir=y -execdir=$out/bin -targetdir=$out/libexec/Mathematica -silent
-  '';
+    # $src is a self-extracting archive
+    # Find the command which computes the offset and create a shell
+    # command to calculate and print it when evaluated.
+    getOffset="$(grep -axm1 -e 'offset=.*' $src); echo \$offset"
 
-  preFixup = ''
-    echo "=== PatchElfing away ==="
-    # This code should be a bit forgiving of errors, unfortunately
-    set +e
-    find $out/libexec/Mathematica/SystemFiles -type f -perm -0100 | while read f; do
-      type=$(readelf -h "$f" 2>/dev/null | grep 'Type:' | sed -e 's/ *Type: *\([A-Z]*\) (.*/\1/')
-      if [ -z "$type" ]; then
-        :
-      elif [ "$type" == "EXEC" ]; then
-        echo "patching $f executable <<"
-        patchelf --shrink-rpath "$f"
-        patchelf \
-	  --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
-          --set-rpath "$(patchelf --print-rpath "$f"):${ldpath}" \
-          "$f" \
-          && patchelf --shrink-rpath "$f" \
-          || echo unable to patch ... ignoring 1>&2
-      elif [ "$type" == "DYN" ]; then
-        echo "patching $f library <<"
-        patchelf \
-          --set-rpath "$(patchelf --print-rpath "$f"):${ldpath}" \
-          "$f" \
-          && patchelf --shrink-rpath "$f" \
-          || echo unable to patch ... ignoring 1>&2
-      else
-        echo "not patching $f <<: unknown elf type"
-      fi
+    # Run $getOffset in another shell.
+    # The command assumes that $0 contains the path of the
+    # self-extracting archive.
+    offset=$(''${SHELL:?} -c "$getOffset" "$src")
+
+    # Extract the archive into `$out/tmp'
+    # The file is HUGE and $TMP might be in RAM
+    mkdir -p "$out/tmp"
+    cd "$out/tmp"
+
+    dd if="$src" ibs=$offset skip=1 2>/dev/null | tar -xf - \
+      && echo " done" \
+      || ( echo " failed" && exit 1 )
+
+    cd Unix/Installer
+
+    # Don't restrict PATH
+    # Nix has already done that
+    sed -i MathInstaller -e '/^PATH=/ s|^|# |'
+    # Replace /bin/bash with the current shell
+    sed -i MathInstaller -e "s|/bin/bash|''${SHELL:?}|"
+
+    echo -n "Installing..."
+    ./MathInstaller \
+        -auto \
+        -createdir=y \
+        -execdir="$out/bin" \
+        -targetdir="$out/libexec/Mathematica" \
+        -silent \
+      && echo " done" \
+      || ( echo " failed" && exit 1 )
+
+    echo -n "Removing installer files..."
+    cd $NIX_BUILD_TOP
+    rm -fr "$out/tmp" && echo " done" || ( echo " failed" && exit 1 )
+
+    echo "Patching ELF objects in \`$out/libexec/Mathematica/SystemFiles'..."
+    (
+      set +e
+
+      cd "$out/libexec/Mathematica/SystemFiles"
+
+      # This code should be a bit forgiving of errors, unfortunately
+      find -type f -perm -0100 | while read f; do
+        type=$(
+          readelf -h "$f" 2>/dev/null \
+          | grep 'Type:' \
+          | sed -e 's/ *Type: *\([A-Z]*\) (.*/\1/'
+        )
+        case "$type" in
+          EXEC)
+            echo -n "  patching executable \`$f'..."
+            patchelf --shrink-rpath "$f"
+            rpath=$(patchelf --print-rpath "$f")
+            patchelf \
+                --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+                --set-rpath "$rpath''${rpath:+:}$ldpath" \
+                "$f" \
+              && patchelf --shrink-rpath "$f" \
+              && echo " done" \
+              || echo " failed (ignored)"
+            ;;
+          DYN)
+            echo -n "  patching library \`$f'..."
+            patchelf --shrink-rpath "$f"
+            rpath=$(patchelf --print-rpath "$f")
+            patchelf --set-rpath "$rpath''${rpath:+:}$ldpath" "$f" \
+              && patchelf --shrink-rpath "$f" \
+              && echo " done" \
+              || echo " failed (ignored)"
+            ;;
+          *)
+            if [ -n "$type" ]; then
+              echo "  skipping \`$f': not a dynamic ELF object"
+            fi
+            ;;
+        esac
+      done
+    )
+
+    install="$out/libexec/Mathematica/SystemFiles/Installation"
+    resources="$out/libexec/Mathematica/SystemFiles/FrontEnd/SystemResources/X"
+
+    echo "Installing icons..."
+    # Install application icons
+    for sz in 32 64 128
+    do
+      dest="$out/share/icons/hicolor/''${sz}x''${sz}"
+      # Application icons
+      mkdir -p "$dest/apps"
+      cp "$resources/App-$sz.png" "$dest/apps/wolfram-mathematica.png"
+      # MIME type icons
+      mkdir -p "$dest/mimetypes"
+      for typ in cdf mathematica.package nb player wl
+      do
+        cp "$resources/vnd.wolfram.$typ-$sz.png" \
+          "$dest/mimetypes/application-vnd.wolfram.$typ.png"
+      done
     done
+    echo "Installing icons... done"
+
+    echo "Installing menu entry..."
+    mkdir -p "$out/share/applications"
+    # Do not use absolute paths in desktop entry
+    sed -e "/Exec=/ s|$out/libexec/Mathematica/Executables/||" \
+        -e '/^\[Desktop/ a Categories=Education;Science;Math;' \
+        "$install/wolfram-mathematica11.desktop" \
+        >"$out/share/applications/wolfram-mathematica11.desktop"
+    echo "Installing menu entry... done"
+
+    echo "Installing MIME types..."
+    mkdir -p "$out/share/mime/packages"
+    for typ in cdf mathematica.package nb nbp wl wpl
+    do
+      cp "$install/application-vnd.wolfram.$typ.xml" "$out/share/mime/packages"
+    done
+    echo "Installing MIME types... done"
   '';
-
-  # all binaries are already stripped
-  dontStrip = true;
-
-  # we did this in prefixup already
-  dontPatchELF = true;
 
   meta = {
     description = "Wolfram Mathematica computational software system";
     homepage = "http://www.wolfram.com/mathematica/";
     license = stdenv.lib.licenses.unfree;
+    platforms = [ "i686-linux" "x86_64-linux" ];
   };
 }
