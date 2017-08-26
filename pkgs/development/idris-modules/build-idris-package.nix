cc5a0a84e8da5bfe5d194533fa5bb7c8ce186094
Thomas Tuegel <ttuegel@mailbox.org>
2017-08-26 10:43:59 -0500

    build-idris-package: run pre- and post-phase hooks

--- pkgs/development/idris-modules/build-idris-package.nix
+++ pkgs/development/idris-modules/build-idris-package.nix
@@ -24,19 +24,31 @@ stdenv.mkDerivation ({
     export IBCSUBDIR=$out/lib/${idris.name}
     mkdir -p $IBCSUBDIR
 
+    runHook preBuild
+
     ${idris}/bin/idris --build *.ipkg
+
+    runHook postBuild
   '';
 
   doCheck = true;
 
   checkPhase = ''
+    runHook preCheck
+
     if grep -q test *.ipkg; then
       ${idris}/bin/idris --testpkg *.ipkg
     fi
+
+    runHook postCheck
   '';
 
   installPhase = ''
+    runHook preInstall
+
     ${idris}/bin/idris --install *.ipkg --ibcsubdir $IBCSUBDIR
+
+    runHook postInstall
   '';
 
   buildInputs = [ gmp ];
