8511cc5e4091f43d1ff7966f7daca453a722caf2
Thomas Tuegel <ttuegel@mailbox.org>
2017-11-26 20:01:39 -0600

    otfcc: generalize platforms to build on linux and darwin

--- pkgs/tools/misc/otfcc/default.nix
+++ pkgs/tools/misc/otfcc/default.nix
@@ -32,7 +32,7 @@ stdenv.mkDerivation rec {
     description = "Optimized OpenType builder and inspector";
     homepage = https://github.com/caryll/otfcc;
     license = licenses.asl20;
-    platforms = [ "i686-linux" "x86_64-linux" "x86_64-darwin" ];
+    platforms = with platforms; [ linux darwin ];
     maintainers = with maintainers; [ jfrankenau ttuegel ];
   };
 
