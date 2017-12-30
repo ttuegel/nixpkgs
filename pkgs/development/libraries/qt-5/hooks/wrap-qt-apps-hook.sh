bf2fcbf3f68dfe3f3d0053068a53253013a606e0
Thomas Tuegel <ttuegel@mailbox.org>
2017-12-30 11:37:26 -0600

    wrapQtAppsHook: run automatically like wrapGAppsHook

--- pkgs/development/libraries/qt-5/hooks/wrap-qt-apps-hook.sh
+++ pkgs/development/libraries/qt-5/hooks/wrap-qt-apps-hook.sh
@@ -1,7 +1,7 @@
 host_QT_PLUGIN_PATH=
 host_QML2_IMPORT_PATH=
 
-qtWrapperArgs=()
+qtWrapperArgs=(--unset QT_PLUGIN_PATH --unset QML2_IMPORT_PATH)
 
 qtHostPathHook() {
     local pluginDir="$1/$qtPluginPrefix"
@@ -36,3 +36,29 @@ wrapQtApp() {
     shift 1
     wrapProgram "$program" "${qtWrapperArgs[@]}" "$@"
 }
+
+# Note: $qtWrapperArgs still gets defined even if $dontWrapGApps is set.
+wrapQtAppsHook() {
+  # guard against running multiple times (e.g. due to propagation)
+  [ -z "$wrapQtAppsHookHasRun" ] || return 0
+  wrapQtAppsHookHasRun=1
+
+  if [ -d "$prefix/share" ]; then
+    qtWrapperArgs+=(--prefix XDG_DATA_DIRS : "$prefix/share")
+  fi
+
+  if [[ -z "$dontWrapQtApps" ]]; then
+    local targetDirs=( "$prefix/bin" "$prefix/libexec" )
+    for targetDir in "${targetDirs[@]}"; do
+      if [ -d "$targetDir" ]; then
+        find -L "$targetDir" -type f -executable -print0 \
+          | while IFS= read -r -d '' file; do
+          echo "Wrapping program $file"
+          wrapQtApp "$file"
+        done
+      fi
+    done
+  fi
+}
+
+fixupOutputHooks+=(wrapQtAppsHook)
