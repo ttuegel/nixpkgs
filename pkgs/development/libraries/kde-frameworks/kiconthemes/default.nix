2ca7c311d95457547f6f3249bd634ea451d4f1eb
Thomas Tuegel <ttuegel@mailbox.org>
2018-01-18 10:43:46 -0600

    kiconthemes: do not use copyPathsToStore

--- pkgs/development/libraries/kde-frameworks/kiconthemes/default.nix
+++ pkgs/development/libraries/kde-frameworks/kiconthemes/default.nix
@@ -1,6 +1,6 @@
 {
-  mkDerivation, lib, copyPathsToStore,
   extra-cmake-modules,
+  mkDerivation, lib,
   breeze-icons, karchive, kcoreaddons, kconfigwidgets, ki18n, kitemviews,
   qtbase, qtsvg,
 }:
@@ -8,8 +8,8 @@
 mkDerivation {
   name = "kiconthemes";
   meta = { maintainers = [ lib.maintainers.ttuegel ]; };
-  patches = copyPathsToStore (lib.readPathsFromFile ./. ./series);
   nativeBuildInputs = [ extra-cmake-modules ];
+  patches = [ ./default-theme-breeze.patch ];
   buildInputs = [
     breeze-icons karchive kcoreaddons kconfigwidgets ki18n kitemviews
   ];
