2facbc053d42da75f0cc5ad418dfa41e522e313c
Thomas Tuegel <ttuegel@mailbox.org>
2018-01-18 11:46:05 -0600

    kross: wrapQtAppsHook

--- pkgs/development/libraries/kde-frameworks/kross.nix
+++ pkgs/development/libraries/kde-frameworks/kross.nix
@@ -1,12 +1,14 @@
-{ mkDerivation, lib, extra-cmake-modules, kcompletion, kcoreaddons
-, kdoctools, ki18n, kiconthemes, kio, kparts, kwidgetsaddons
-, kxmlgui, qtbase, qtscript, qtxmlpatterns,
+{
+  mkDerivation, lib,
+  extra-cmake-modules, kdoctools, wrapQtAppsHook,
+  kcompletion, kcoreaddons, ki18n, kiconthemes, kio, kparts, kwidgetsaddons,
+  kxmlgui, qtbase, qtscript, qtxmlpatterns,
 }:
 
 mkDerivation {
   name = "kross";
   meta = { maintainers = [ lib.maintainers.ttuegel ]; };
-  nativeBuildInputs = [ extra-cmake-modules kdoctools ];
+  nativeBuildInputs = [ extra-cmake-modules kdoctools wrapQtAppsHook ];
   buildInputs = [ kcompletion kcoreaddons kxmlgui ];
   propagatedBuildInputs = [
     ki18n kiconthemes kio kparts kwidgetsaddons qtbase qtscript qtxmlpatterns
