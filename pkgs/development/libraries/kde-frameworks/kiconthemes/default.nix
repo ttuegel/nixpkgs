6f377990b6d162c560fa31a8a877c47af9879888
Thomas Tuegel <ttuegel@mailbox.org>
2018-01-18 10:44:02 -0600

    kiconthemes: wrapQtAppsHook

--- pkgs/development/libraries/kde-frameworks/kiconthemes/default.nix
+++ pkgs/development/libraries/kde-frameworks/kiconthemes/default.nix
@@ -1,6 +1,6 @@
 {
-  extra-cmake-modules,
   mkDerivation, lib,
+  extra-cmake-modules, wrapQtAppsHook,
   breeze-icons, karchive, kcoreaddons, kconfigwidgets, ki18n, kitemviews,
   qtbase, qtsvg,
 }:
@@ -8,8 +8,8 @@
 mkDerivation {
   name = "kiconthemes";
   meta = { maintainers = [ lib.maintainers.ttuegel ]; };
-  nativeBuildInputs = [ extra-cmake-modules ];
   patches = [ ./default-theme-breeze.patch ];
+  nativeBuildInputs = [ extra-cmake-modules wrapQtAppsHook ];
   buildInputs = [
     breeze-icons karchive kcoreaddons kconfigwidgets ki18n kitemviews
   ];
