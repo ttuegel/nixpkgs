27f0c901384767786d5fcdfa7096e32172ed2dfd
Thomas Tuegel <ttuegel@mailbox.org>
2018-01-18 11:35:04 -0600

    kjsembed: wrapQtAppsHook

--- pkgs/development/libraries/kde-frameworks/kjsembed.nix
+++ pkgs/development/libraries/kde-frameworks/kjsembed.nix
@@ -1,13 +1,13 @@
 {
   mkDerivation, lib,
-  extra-cmake-modules, kdoctools, qttools,
+  extra-cmake-modules, kdoctools, qttools, wrapQtAppsHook,
   ki18n, kjs, qtsvg,
 }:
 
 mkDerivation {
   name = "kjsembed";
   meta = { maintainers = [ lib.maintainers.ttuegel ]; };
-  nativeBuildInputs = [ extra-cmake-modules kdoctools qttools ];
+  nativeBuildInputs = [ extra-cmake-modules kdoctools qttools wrapQtAppsHook ];
   buildInputs = [ ki18n qtsvg ];
   propagatedBuildInputs = [ kjs ];
 }
