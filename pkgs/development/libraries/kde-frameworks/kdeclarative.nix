ce6d6301de158d78d313457279a8a05f25e29fe7
Thomas Tuegel <ttuegel@mailbox.org>
2018-01-18 07:12:59 -0600

    kdeclarative: wrapQtAppsHook

--- pkgs/development/libraries/kde-frameworks/kdeclarative.nix
+++ pkgs/development/libraries/kde-frameworks/kdeclarative.nix
@@ -1,5 +1,5 @@
 {
-  mkDerivation, lib, extra-cmake-modules,
+  mkDerivation, lib, extra-cmake-modules, wrapQtAppsHook,
   epoxy, kconfig, kglobalaccel, kguiaddons, ki18n, kiconthemes, kio, kpackage,
   kwidgetsaddons, kwindowsystem, pkgconfig, qtdeclarative
 }:
@@ -7,7 +7,7 @@
 mkDerivation {
   name = "kdeclarative";
   meta = { maintainers = [ lib.maintainers.ttuegel ]; };
-  nativeBuildInputs = [ extra-cmake-modules ];
+  nativeBuildInputs = [ extra-cmake-modules wrapQtAppsHook ];
   buildInputs = [
     epoxy kglobalaccel kguiaddons ki18n kiconthemes kio kwidgetsaddons
     kwindowsystem
