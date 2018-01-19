33a78609db13ccd7562fd6965d3d52ac2a0a99c6
Thomas Tuegel <ttuegel@mailbox.org>
2018-01-19 10:14:30 -0600

    ksshaskpass: wrapQtAppsHook

--- pkgs/desktops/plasma-5/ksshaskpass.nix
+++ pkgs/desktops/plasma-5/ksshaskpass.nix
@@ -1,11 +1,11 @@
 {
   mkDerivation,
-  extra-cmake-modules, kdoctools,
+  extra-cmake-modules, kdoctools, wrapQtAppsHook,
   kcoreaddons, ki18n, kwallet, kwidgetsaddons, qtbase,
 }:
 
 mkDerivation {
   name = "ksshaskpass";
-  nativeBuildInputs = [ extra-cmake-modules kdoctools ];
+  nativeBuildInputs = [ extra-cmake-modules kdoctools wrapQtAppsHook ];
   buildInputs = [ kcoreaddons ki18n kwallet kwidgetsaddons qtbase ];
 }
