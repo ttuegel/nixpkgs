8dc2347c7cec3df9424e0387ebe64534953a964a
Thomas Tuegel <ttuegel@mailbox.org>
2018-01-19 13:21:10 -0600

    kwrited: wrapQtAppsHook

--- pkgs/desktops/plasma-5/kwrited.nix
+++ pkgs/desktops/plasma-5/kwrited.nix
@@ -1,11 +1,11 @@
 {
   mkDerivation,
-  extra-cmake-modules,
+  extra-cmake-modules, wrapQtAppsHook,
   kcoreaddons, kdbusaddons, ki18n, knotifications, kpty, qtbase,
 }:
 
 mkDerivation {
   name = "kwrited";
-  nativeBuildInputs = [ extra-cmake-modules ];
+  nativeBuildInputs = [ extra-cmake-modules wrapQtAppsHook ];
   buildInputs = [ kcoreaddons kdbusaddons ki18n knotifications kpty qtbase ];
 }
