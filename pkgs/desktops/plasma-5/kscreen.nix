09ce57b724961b6fe6d2ac3ff20f26086a3048cd
Thomas Tuegel <ttuegel@mailbox.org>
2018-01-19 10:12:53 -0600

    kscreen: wrapQtAppsHook

--- pkgs/desktops/plasma-5/kscreen.nix
+++ pkgs/desktops/plasma-5/kscreen.nix
@@ -1,13 +1,13 @@
 {
   mkDerivation,
-  extra-cmake-modules,
+  extra-cmake-modules, wrapQtAppsHook,
   kconfig, kconfigwidgets, kdbusaddons, kglobalaccel, ki18n, kwidgetsaddons,
   kxmlgui, libkscreen, qtdeclarative, qtgraphicaleffects
 }:
 
 mkDerivation {
   name = "kscreen";
-  nativeBuildInputs = [ extra-cmake-modules ];
+  nativeBuildInputs = [ extra-cmake-modules wrapQtAppsHook ];
   buildInputs = [
     kconfig kconfigwidgets kdbusaddons kglobalaccel ki18n kwidgetsaddons kxmlgui
     libkscreen qtdeclarative qtgraphicaleffects
