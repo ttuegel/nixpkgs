b571020cdade167631dd3006e7d700c355a6eefa
Thomas Tuegel <ttuegel@mailbox.org>
2018-01-19 05:50:22 -0600

    kinfocenter: wrapQtAppsHook

--- pkgs/desktops/plasma-5/kinfocenter.nix
+++ pkgs/desktops/plasma-5/kinfocenter.nix
@@ -1,6 +1,6 @@
 {
   mkDerivation,
-  extra-cmake-modules, kdoctools,
+  extra-cmake-modules, kdoctools, wrapQtAppsHook,
   kcmutils, kcompletion, kconfig, kconfigwidgets, kcoreaddons, kdbusaddons,
   kdeclarative, kdelibs4support, ki18n, kiconthemes, kio, kpackage, kservice,
   kwayland, kwidgetsaddons, kxmlgui, libraw1394, mesa_glu, pciutils, solid
@@ -8,7 +8,7 @@
 
 mkDerivation {
   name = "kinfocenter";
-  nativeBuildInputs = [ extra-cmake-modules kdoctools ];
+  nativeBuildInputs = [ extra-cmake-modules kdoctools wrapQtAppsHook ];
   buildInputs = [
     kcmutils kcompletion kconfig kconfigwidgets kcoreaddons kdbusaddons
     kdeclarative kdelibs4support ki18n kiconthemes kio kpackage kservice
