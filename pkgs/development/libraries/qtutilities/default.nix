d94ca009c17dc45d8ddbaad45017d3a240ce43c0
Thomas Tuegel <ttuegel@mailbox.org>
2021-01-15 05:24:05 -0600

    qtutilities: dontWrapQtApps

--- pkgs/development/libraries/qtutilities/default.nix
+++ pkgs/development/libraries/qtutilities/default.nix
@@ -14,6 +14,8 @@ stdenv.mkDerivation rec {
   buildInputs = [ qtbase cpp-utilities ];
   nativeBuildInputs = [ cmake qttools ];
 
+  dontWrapQtApps = true;
+
   meta = with stdenv.lib; {
     homepage = "https://github.com/Martchus/qtutilities";
     description = "Common C++ classes and routines used by @Martchus' applications featuring argument parser, IO and conversion utilities";
