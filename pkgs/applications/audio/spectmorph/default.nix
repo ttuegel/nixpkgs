57e2dc6641a86cc1b79430329bfd821a5e1513fb
Thomas Tuegel <ttuegel@mailbox.org>
2021-01-17 05:31:18 -0600

    spectmorph: dontWrapQtApps

--- pkgs/applications/audio/spectmorph/default.nix
+++ pkgs/applications/audio/spectmorph/default.nix
@@ -12,6 +12,8 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ pkgconfig ];
 
+  dontWrapQtApps = true;
+
   meta = with lib; {
     description = "Allows to analyze samples of musical instruments, and to combine them (morphing) to construct hybrid sounds";
     homepage = "http://spectmorph.org";
