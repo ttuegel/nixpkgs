0aacecd502a2056421a703652b9bf154ca19b2a1
Thomas Tuegel <ttuegel@mailbox.org>
2021-03-28 06:32:24 -0500

    kquickcharts: mark broken with Qt < 5.14

--- pkgs/development/libraries/kde-frameworks/kquickcharts.nix
+++ pkgs/development/libraries/kde-frameworks/kquickcharts.nix
@@ -1,12 +1,13 @@
 {
   mkDerivation, lib,
   extra-cmake-modules,
-  qtquickcontrols2,
+  qtquickcontrols2, qtbase,
 }:
 
 mkDerivation {
   name = "kquickcharts";
   meta = {
+    broken = lib.versionOlder qtbase.version "5.14";
     maintainers = [ lib.maintainers.ttuegel ];
   };
   nativeBuildInputs = [ extra-cmake-modules ];
